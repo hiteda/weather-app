d0b006f29b6161a243296c30943a3dfa1410de9e
David K Hite <david.k.hite@gmail.com>
2021-04-22 21:42:55 -0700

    Align forecasts vertically

--- src/components/Day.js
+++ src/components/Day.js
@@ -4,7 +4,7 @@ import { getWeekdayFromIndex, getDateFromIndex, weather } from '../utils/weather
 const Day = ({day, index}) => {
     const weather = getWeatherFromDescription(day.weather[0].description);
     return ( 
-        <div className="list-group-item">
+        <div className="list-group-item col-sm-2" style={{ 'maxWidth': '169px' }} >
             <h4 className="d-flex w-100">{getWeekdayFromIndex(index)}</h4>
             <h5 className="d-flex w-100">({getDateFromIndex(index)})</h5>
             <img src={weather.icon} />
